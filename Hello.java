61b91b1de4ab81127027f89ef23868054c190b2c
Ranjol <23251a1225@gmail.com>
2026-08-05 10:10:52 +0530

    initial commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Hello{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hello world");
+        System.out.println("Welcome jenkins");
+    }
+}
